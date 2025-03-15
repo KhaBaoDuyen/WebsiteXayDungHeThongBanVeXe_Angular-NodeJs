2546969dd7991948aa65c707a31e77f8aef5aa4d
KhaBaoDuyen <khathibaoduyen9@gmail.com>
2025-03-15 21:32:45 +0700

    Khởi tạo repository

---
+++ src/app/pages/ui-components/busRoute/bus-create/bus-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bus-create',
+  imports: [],
+  templateUrl: './bus-create.component.html',
+  styleUrl: './bus-create.component.scss'
+})
+export class BusCreateComponent {
+
+}
