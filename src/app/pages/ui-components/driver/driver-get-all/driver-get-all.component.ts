862bf91bd7f0500c7341d9c4071b2816109fec79
KhaBaoDuyen <khathibaoduyen9@gmail.com>
2025-03-18 00:57:35 +0700

    [ FE-ADMIN ] - Node2.adm.getAll.formCreare.driver

--- src/app/pages/ui-components/driver/driver-get-all/driver-get-all.component.ts
+++ src/app/pages/ui-components/driver/driver-get-all/driver-get-all.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+import { driveriInterface } from '../../../../interface/driver.interface';
 
 @Component({
   selector: 'app-driver-get-all',
-  imports: [],
+  standalone: true,
+  imports: [MatTableModule, MatButtonModule, MatIconModule, CommonModule, MatPaginatorModule],
   templateUrl: './driver-get-all.component.html',
-  styleUrl: './driver-get-all.component.scss'
 })
 export class DriverGetAllComponent {
-
+  displayedColumns: string[] = ['id', 'image', 'fullName', 'phone', 'licenseNumber', 'experienceYears', 'birthDate', 'status', 'createdAt', 'age', 'actions'];
+  dataSource = new MatTableDataSource<driveriInterface>([
+    {
+      id: 1,
+      fullName: "Nguyễn Văn A", 
+      image: "./assets/images/profile/user-6.jpg",
+      phone: "0987654321",
+      licenseNumber: "B123456",
+      experienceYears: 5,
+      birthDate: "1990-05-20",
+      status: "active",
+      createdAt: "2023-05-15T10:00:00",
+      age: 34,
+     
+    }
+  ]);
 }
