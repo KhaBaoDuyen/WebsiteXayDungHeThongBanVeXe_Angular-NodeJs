807c2051826643e48554a488e94f998efd504d8f
huynhnhule2004 <huynhnhule2004@gmail.com>
2025-03-18 20:04:31 +0700

    [ FE_ADMIN ] Node.9.adm.getAll.busType

--- src/app/pages/ui-components/bustype/bustype-get-all/bustype-get-all.component.ts
+++ src/app/pages/ui-components/bustype/bustype-get-all/bustype-get-all.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { BusTypeInterface } from 'src/app/interface/bus-type.interface';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-bustype-get-all',
-  imports: [],
+  standalone: true,
   templateUrl: './bustype-get-all.component.html',
-  styleUrl: './bustype-get-all.component.scss'
+  styleUrls: ['./bustype-get-all.component.scss'],
+  imports: [MatTableModule, MatButtonModule, MatIconModule, CommonModule, MatPaginatorModule, RouterModule],
 })
 export class BustypeGetAllComponent {
-
+  displayedColumns: string[] = ['id', 'name', 'status', 'actions'];
+  dataSource = new MatTableDataSource<BusTypeInterface>([
+    { id: 1, name: "Xe Buýt Thành Phố", status: "active" },
+    { id: 2, name: "Xe Buýt Cao Cấp", status: "inactive" }
+  ]);
 }
