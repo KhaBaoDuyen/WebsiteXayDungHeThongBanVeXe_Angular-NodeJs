c24c74b26c93aaa76e31f9b9c2bdf7a65611efa2
quynh0407 <quynh0939662390@gmail.com>
2025-03-18 16:10:32 +0700

    [ FE-ADMIN ] - node.adm.getAll/formCreate/Edit.routes

--- src/app/pages/ui-components/routes/routes-create/routes-create.component.ts
+++ src/app/pages/ui-components/routes/routes-create/routes-create.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from 'src/app/material.module';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-routes-create',
-  imports: [],
+  imports: [MaterialModule, CommonModule, ReactiveFormsModule],
   templateUrl: './routes-create.component.html',
   styleUrl: './routes-create.component.scss'
 })
 export class RoutesCreateComponent {
+driverForm: FormGroup;
+  selectedFile: File | null = null;
+
 
 }
