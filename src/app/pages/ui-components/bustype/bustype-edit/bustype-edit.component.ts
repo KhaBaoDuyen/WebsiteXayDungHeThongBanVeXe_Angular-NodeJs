36ed18f4a29cd5205b8ddbb80c0e419f17e53e77
KhaBaoDuyen <khathibaoduyen9@gmail.com>
2025-03-20 21:05:55 +0700

    [ FE-ADMIN] - invalid.busType

--- src/app/pages/ui-components/bustype/bustype-edit/bustype-edit.component.ts
+++ src/app/pages/ui-components/bustype/bustype-edit/bustype-edit.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { BusTypeInterface } from 'src/app/interface/bus-type.interface';
 
 @Component({
   selector: 'app-bustype-edit',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],  
   templateUrl: './bustype-edit.component.html',
   styleUrl: './bustype-edit.component.scss'
 })
 export class BustypeEditComponent {
   bustype: BusTypeInterface = {
     id: 1,
-    name: "Xe Buýt Thành Phố", 
+    name: "Xe Buýt Thành Phố",
     status: "active"
   };
+
+  name = new FormControl(this.bustype.name, [Validators.required, Validators.minLength(5)]);
+  status = new FormControl(this.bustype.status);
+
+  onSave() {
+    Object.values({ name: this.name, status: this.status }).forEach(control => control.markAsTouched())
+     return;
+  }
 }
