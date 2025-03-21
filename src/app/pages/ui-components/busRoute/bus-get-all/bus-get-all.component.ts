d35953b77640dd1e4235e4580220ca1d3a5316b0
quynh0407 <quynh0939662390@gmail.com>
2025-03-21 20:45:42 +0700

    [ FE_ADMIN ] - note23.adm.invalid-delete.BusRoute/Buses

--- src/app/pages/ui-components/busRoute/bus-get-all/bus-get-all.component.ts
+++ src/app/pages/ui-components/busRoute/bus-get-all/bus-get-all.component.ts
@@ -6,41 +6,58 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { BusRoute } from '../../../../interface/bus-route.interface';
 import { RouterModule } from '@angular/router';
+import { FormDeleteComponent } from 'src/app/components/form-delete/form-delete.component';
 
 @Component({
   selector: 'app-bus-get-all',
   templateUrl: './bus-get-all.component.html',
   styleUrls: ['./bus-get-all.component.scss'],  
   standalone: true,
-  imports: [MatTableModule, MatButtonModule, MatIconModule, CommonModule, MatPaginatorModule, RouterModule],
+  imports: [
+    MatTableModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    CommonModule, 
+    MatPaginatorModule, 
+    RouterModule,
+    FormDeleteComponent,
+  ],
 })
 export class BusGetAllComponent implements AfterViewInit {
   displayedColumns: string[] = ['id', 'route', 'departure', 'arrival', 'price', 'status', 'actions'];
-  dataSource = new MatTableDataSource<BusRoute>([]);
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  busRoutes: BusRoute[] = [
+  dataSource = new MatTableDataSource<BusRoute>([
     { id: 1, route: 'TP.HCM - Hà Nội', departure: '06:00', arrival: '18:00', price: 500000, status: 'Đang chạy' },
     { id: 2, route: 'Đà Nẵng - Hải Phòng', departure: '08:00', arrival: '20:00', price: 450000, status: 'Sắp khởi hành' },
     { id: 3, route: 'Cần Thơ - Nha Trang', departure: '07:30', arrival: '19:00', price: 400000, status: 'Hủy' },
-  ];
+  ]);
 
-  constructor() {
-    this.dataSource.data = this.busRoutes;
-  }
+
+  showDeleteConfirmation = false; 
+  selectedRouteId: number | null = null; 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
-  editRoute(route: BusRoute) {
-    console.log('Chỉnh sửa chuyến xe:', route);
+  // Mở form xác nhận xóa
+  openDeleteConfirmation(RouteId: number) {
+    this.selectedRouteId = RouteId;
+    this.showDeleteConfirmation = true;
   }
 
-  deleteRoute(id: number) {
-    this.busRoutes = this.busRoutes.filter(route => route.id !== id);
-    this.dataSource.data = this.busRoutes;
+  // Xử lý khi xác nhận xóa
+  handleDeleteConfirmed() {
+    if (this.selectedRouteId !== null) {
+      this.dataSource.data = this.dataSource.data.filter(
+        (route) => route.id !== this.selectedRouteId
+      );
+    }
+    this.showDeleteConfirmation = false; 
+  }
+
+  handleCancel() {
+    this.showDeleteConfirmation = false; 
   }
 }
 
--- src/app/pages/ui-components/buses/buses-edit/buses-edit.component.ts
+++ src/app/pages/ui-components/buses/buses-edit/buses-edit.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { busesInterface } from '../../../../interface/buses.interface';
+import { FormBuilder, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-buses-edit',
-  imports: [],
+  imports: [ ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './buses-edit.component.html',
   styleUrl: './buses-edit.component.scss'
 })
@@ -16,4 +20,16 @@ export class BusesEditComponent {
     totalSeats: 40,
     seatsId: 1
   };
+
+  plateNumber = new FormControl(this.buses.plateNumber, [Validators.required,  Validators.minLength(5)]);
+  busTypeID = new FormControl(this.buses.busTypeID, Validators.required);
+  driverId = new FormControl(this.buses.driverId, Validators.required);
+  totalSeats = new FormControl(this.buses.totalSeats, [Validators.required, Validators.pattern('^[0-9]+$')]);
+  status = new FormControl(this.buses.status); 
+  seatsId = new FormControl(this.buses.seatsId, Validators.required);
+
+  onSave() {
+    Object.values({ plateNumber: this.plateNumber, busTypeID: this.busTypeID, driverId: this.driverId, totalSeats: this.totalSeats, seatsId: this.seatsId }).forEach(control => control.markAsTouched())
+     return;
+  }
 }
