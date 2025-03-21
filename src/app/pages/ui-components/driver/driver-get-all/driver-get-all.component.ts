943d006cebf5406f2b7f389777abb988be7a47b1
KhaBaoDuyen <khathibaoduyen9@gmail.com>
2025-03-21 14:22:28 +0700

    [ FE_ADMIN ] - Note21.adm.getAll.Driver

--- src/app/pages/ui-components/driver/driver-get-all/driver-get-all.component.ts
+++ src/app/pages/ui-components/driver/driver-get-all/driver-get-all.component.ts
@@ -4,14 +4,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RouterModule } from '@angular/router';
 
 import { driveriInterface } from '../../../../interface/driver.interface';
-import { RouterModule } from '@angular/router';
+import { FormDeleteComponent } from 'src/app/components/form-delete/form-delete.component';
+
+
 
 @Component({
   selector: 'app-driver-get-all',
   standalone: true,
-  imports: [RouterModule, MatTableModule, MatButtonModule, MatIconModule, CommonModule, MatPaginatorModule],
+  imports: [RouterModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    MatPaginatorModule,
+    FormDeleteComponent],
   templateUrl: './driver-get-all.component.html',
 })
 export class DriverGetAllComponent {
@@ -19,7 +28,7 @@ export class DriverGetAllComponent {
   dataSource = new MatTableDataSource<driveriInterface>([
     {
       id: 1,
-      fullName: "Nguyễn Văn A", 
+      fullName: "Nguyễn Văn A",
       image: "./assets/images/profile/user-6.jpg",
       phone: "0987654321",
       licenseNumber: "B123456",
@@ -28,7 +37,30 @@ export class DriverGetAllComponent {
       status: "active",
       createdAt: "2023-05-15T10:00:00",
       age: 34,
-     
+
     }
   ]);
+
+  showFormDelete = false;
+  driverId: number | null = null;
+
+  openDeleteConfirmation(driverId: number) {
+    this.driverId = driverId;
+    this.showFormDelete = true;
+  }
+
+  // Xử lý khi xác nhận xóa
+  handleDeleteConfirmed() {
+    if (this.driverId !== null) {
+      this.dataSource.data = this.dataSource.data.filter(
+        (busType) => busType.id !== this.driverId
+      );
+    }
+    this.showFormDelete = false;
+  }
+
+  handleCancel() {
+    this.showFormDelete = false;
+  }
+
 }
