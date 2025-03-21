7c1f9f8453bfe5d84e6722fc737d217e2035d4d2
quynh0407 <quynh0939662390@gmail.com>
2025-03-21 20:45:42 +0700

    [ FE_ADMIN ] - note23.adm.invalid-delete.BusRoute/Buses

--- src/app/pages/ui-components/busRoute/bus-create/bus-create.component.ts
+++ src/app/pages/ui-components/busRoute/bus-create/bus-create.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from 'src/app/material.module';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bus-create',
-  imports: [MaterialModule, CommonModule, ReactiveFormsModule],
+  imports: [MaterialModule, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './bus-create.component.html',
   styleUrl: './bus-create.component.scss'
 })
 export class BusCreateComponent {
-  driverForm: FormGroup;
-  selectedFile: File | null = null;
+  route = new FormControl('', [Validators.required,  Validators.minLength(5)]);
+  departure = new FormControl('', Validators.required);
+  arrival = new FormControl('', Validators.required);
+  price = new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]);
+  status = new FormControl('active'); 
+
+  onSave() {
+    const controls = { route: this.route, departure: this.departure, arrival: this.arrival, price: this.price };
+    Object.values(controls).forEach(control =>  control.markAsTouched());
+      return;
+    }
 }
