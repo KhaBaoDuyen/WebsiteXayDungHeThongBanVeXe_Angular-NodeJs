afdd2371e16fcdc7fec9d439b794f73771dfe59e
quoclinh07070 <155744993+quoclinh07070@users.noreply.github.com>
2025-03-23 16:33:56 +0700

    Note17_admin_validation_edit_blogs

--- FE/src/app/pages/ui-components/Admin/blog/edit-blogs/edit-blogs.component.ts
+++ FE/src/app/pages/ui-components/Admin/blog/edit-blogs/edit-blogs.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { blogInterface } from 'src/app/interface/blogInterface';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-edit-blogs',
   templateUrl: './edit-blogs.component.html',
   styleUrls: ['./edit-blogs.component.scss'],
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, CKEditorModule],
 })
 export class EditBlogsComponent {
+  blogForm: FormGroup;
+  public Editor: any = ClassicEditorBuild;
   blog: blogInterface = {
     id: 1,
     title: 'Cách học Angular hiệu quả',
@@ -17,4 +25,25 @@ export class EditBlogsComponent {
     status: 'active',
     createAt: '2024-03-21'
   };
+
+  constructor(private fb: FormBuilder) {
+    this.blogForm = this.fb.group({
+      title: [this.blog.title, [Validators.required, Validators.minLength(5)]],
+      status: [this.blog.status, Validators.required],
+      content: [this.blog.content, [Validators.required, Validators.minLength(20)]],
+      image: [null],
+    });
+  }
+
+  get title() { return this.blogForm.get('title'); }
+  get status() { return this.blogForm.get('status'); }
+  get content() { return this.blogForm.get('content'); }
+
+  onSubmit() {
+    if (this.blogForm.invalid) {
+      this.blogForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.blogForm.value);
+  }
 }
