30f0edbe80921573f6e738e3fdbfdd90af3c205b
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2025-03-25 02:57:59 +0700

    [ FE -CLIENT ] - note33.cln.home.header.footer

--- FE/src/app/layouts/Client/client-layout.component.ts
+++ FE/src/app/layouts/Client/client-layout.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { CoreService } from '../../services/core.service'; // Replace 'path-to-core-service' with the actual path
 @Component({
   selector: 'app-client-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,],
   template: `
     <app-header></app-header>
    <router-outlet></router-outlet>
     <app-footer></app-footer>
   `,
 })
-export class ClientLayoutComponent {}
+export class ClientLayoutComponent {
+  
+  constructor(private coreService: CoreService) {}
+
+  openModal() {
+      this.coreService.openCancelModal();
+  }
+
+}
---
+++ FE/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,ts,js,jsx,tsx}", // Đảm bảo các tệp này được quét
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
