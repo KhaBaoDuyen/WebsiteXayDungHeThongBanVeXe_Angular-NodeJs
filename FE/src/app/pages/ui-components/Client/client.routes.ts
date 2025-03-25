38d37b77f4074505f31b3b00badba94c3087d2d6
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2025-03-25 21:38:51 +0700

    [ SRC-CLIENT ]- note35.cln.routerClient

--- FE/src/app/pages/ui-components/Client/client.routes.ts
+++ FE/src/app/pages/ui-components/Client/client.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { BlogComponent } from "./blog/blog.component";
+import { TimetableComponent } from "./timetable/timetable.component";
+import { ContactComponent } from "./contact/contact.component";
+import { TicketHistoryComponent } from "./ticket-history/ticket-history.component";
 
 export const ClientRoutes: Routes = [
   {
@@ -9,6 +14,30 @@ export const ClientRoutes: Routes = [
   },
   {
     path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
+  {
+    path: 'timetable',
+    component: TimetableComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'history',
+    component: TicketHistoryComponent,
+  },
+  {
+    path: '**',
     component: HomeComponent,
   },
 ];
